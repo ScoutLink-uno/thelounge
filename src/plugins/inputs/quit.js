692451e1113011a874889fdb7d379ce7335b13b5
Yorick Bosman <yorick@gewoonyorick.nl>
2020-07-01 19:46:17 +0200

    Disable /quit and add error message

--- src/plugins/inputs/quit.js
+++ src/plugins/inputs/quit.js
@@ -1,25 +1,19 @@
 "use strict";
 
 const _ = require("lodash");
-const ClientCertificate = require("../clientCertificate");
+const Msg = require("../../models/msg");
 
 exports.commands = ["quit"];
 exports.allowDisconnected = true;
 
 exports.input = function (network, chan, cmd, args) {
-	const client = this;
-
-	client.networks = _.without(client.networks, network);
-	network.destroy();
-	client.save();
-	client.emit("quit", {
-		network: network.uuid,
-	});
-
-	const quitMessage = args[0] ? args.join(" ") : null;
-	network.quit(quitMessage);
-
-	ClientCertificate.remove(network.uuid);
-
-	return true;
+	chan.pushMessage(
+		this,
+		new Msg({
+			type: Msg.Type.ERROR,
+			text:
+				"You can not use /quit. Please use /znc disconnect <message> instead to disconnect from a network. Or type /disconnect to disconnect webchat from IRCManager.",
+		})
+	);
+	return;
 };
